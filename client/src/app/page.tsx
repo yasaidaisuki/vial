a815dbeea68c4ac455bb54ee71ccbd456b41c917
yasaidaisuki <peng.dingkai@gmail.com>
2025-05-27 22:07:06 -0400

    pull request, basic viewtable

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,10 +1,19 @@
 import Image from "next/image";
 import TableView from "@/components/TableView";
+import axios from "axios";
 
-export default function Home() {
+export default async function Home() {
+
+  const data = await axios
+        .get("http://localhost:8080/form-data") // your Fastify backend endpoint
+        .then((response) => {
+          return response.data
+        })
+        .catch((err) => console.log("error"));
+
   return (
     <div className="w-full h-full">
-      <TableView/>
+      {data ? <TableView data={data.data}/> : null }
     </div>
   );
 }
--- client/src/components/TableView.tsx
+++ client/src/components/TableView.tsx
@@ -1,19 +1,36 @@
-import React from 'react'
+"use client";
 
-export default function TableView() {
+import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+import axios from "axios";
+
+export default function TableView({ data }: any) {
+  // extract data
+  const [formData, setFormData] = useState<any>(data?.formData);
+
+  console.log(formData);
+
   return (
-    <div className="flex flex-col w-full">
-      <div className='flex w-full flex-row justify-between p-5'>
-        <div className='flex flex-col'>
+    <div className="flex flex-col w-full h-full">
+      <div className="flex w-full flex-row justify-between p-5">
+        <div className="flex flex-col gap-5">
           <label>Fields</label>
+          {formData &&
+          formData.map((dataObject: any, key: number) => {
+            return (
+              <div key={key} className="flex flex-col justify-between max-w-[500px]">
+                {dataObject.question}
+              </div>
+            );
+          })}
         </div>
+        <div className="flex flex-col">CRA</div>
 
-        <div className='flex flex-col'>CRA</div>
+        <div className="flex flex-col">DM</div>
 
-        <div className='flex flex-col'>DM</div>
-
-        <div className='flex flex-col'>Queries</div>
+        <div className="flex flex-col">Queries</div>
       </div>
     </div>
-  )
+  );
 }
