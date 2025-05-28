03a8082f9c2e5cc12a3dd3d5fa17aeab60dd2102
yasaidaisuki <peng.dingkai@gmail.com>
2025-05-27 23:23:03 -0400

    styling changes

--- client/src/components/TableView.tsx
+++ client/src/components/TableView.tsx
@@ -13,23 +13,30 @@ export default function TableView({ data }: any) {
 
   return (
     <div className="flex flex-col w-full h-full">
-      <div className="flex w-full flex-row justify-between p-5">
-        <div className="flex flex-col gap-5">
-          <label>Fields</label>
-          {formData &&
-          formData.map((dataObject: any, key: number) => {
-            return (
-              <div key={key} className="flex flex-col justify-between max-w-[500px]">
-                {dataObject.question}
-              </div>
-            );
-          })}
+      <div className="flex w-full flex-col justify-between gap-5 p-5">
+        <div className="flex flex-row justify-between">
+          <label>FIELDS</label>
+
+          <div className="flex flex-row gap-20 justify-end pr-14">
+            <label>CRA</label>
+            <label>DM</label>
+            <label>QUERIES</label>
+          </div>
         </div>
-        <div className="flex flex-col">CRA</div>
 
-        <div className="flex flex-col">DM</div>
-
-        <div className="flex flex-col">Queries</div>
+        <div className="flex flex-col">
+          {formData &&
+            formData.map((dataObject: any, key: number) => {
+              return (
+                <div
+                  key={key}
+                  className="flex flex-col justify-between max-w-[600px] truncate p-5"
+                >
+                  {dataObject.question}
+                </div>
+              );
+            })}
+        </div>
       </div>
     </div>
   );
