86607d7c8be0ba6c79fec806745a06fb0bafedda
yasaidaisuki <peng.dingkai@gmail.com>
2025-05-29 01:55:31 -0400

    style changes

--- client/src/components/TableView.tsx
+++ client/src/components/TableView.tsx
@@ -12,7 +12,7 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
-import { Textarea } from "@/components/ui/textarea"
+import { Textarea } from "@/components/ui/textarea";
 import { Label } from "@/components/ui/label";
 
 export interface IQueryData {
@@ -59,68 +59,75 @@ export default function TableView({ data }: TableViewProps) {
 
   return (
     <div className="flex flex-col w-full h-full">
-      <div className="flex w-full flex-col justify-between gap-5 p-5">
-        <div className="flex flex-row justify-between md:lg:pl-6 md:lg:pr-6">
-          <label>FIELDS</label>
-          <div className="flex flex-row gap-5 md:lg:gap-28 justify-end">
-            <label>ANSWER</label>
-            <label>QUERIES</label>
+      <div className="flex w-full flex-col justify-between">
+        <div>
+          <div className="flex flex-row justify-between md:lg:pl-6 md:lg:pr-6 p-5">
+            <label className="font-semibold text-stone-500">FIELDS</label>
+            <div className="flex flex-row gap-5 md:lg:gap-36 justify-end">
+              <label className="font-semibold text-stone-500">ANSWER</label>
+              <label className="font-semibold text-stone-500">QUERIES</label>
+            </div>
           </div>
+          <hr className="h-px bg-gray-200 border-0 dark:bg-gray-700"></hr>
         </div>
 
-        <div className="flex flex-col gap-7">
+        <div className="flex flex-col">
           {formData.map((dataObject, key) => (
             <div
               key={key}
-              className="flex flex-col justify-between content-center"
+              className="flex flex-col justify-between content-center py-3 border-[0.1vh] pl-5"
             >
-              <div className="flex flex-row gap-5 md:lg:pl-6 md:lg:pr-6 justify-between">
+
+              <div className="flex flex-row gap-5 pl-2 pr-2 md:lg:pl-6 md:lg:pr-6 justify-between">
                 <div className="max-w-[100px] md:lg:max-w-[500px] truncate content-center">
                   {dataObject.question}
                 </div>
 
-                <div className="flex flex-row gap-10 items-center">
-                  <div className="content-center">{dataObject.answer}</div>
+                <div className="flex flex-row gap-10 items-center justify-between">
+                  <div className="content-center ">{dataObject.answer}</div>
 
-                  <Dialog
-                    open={dialogOpenIndex === key}
-                    onOpenChange={(open) =>
-                      setDialogOpenIndex(open ? key : null)
-                    }
-                  >
-                    <DialogTrigger asChild>
-                      <Button className="w-10 h-10 bg-black text-white hover:bg-gray-600">
-                        +
-                      </Button>
-                    </DialogTrigger>
-                    <DialogContent>
-                      <DialogHeader>
-                        <DialogTitle>Create Query</DialogTitle>
-                        <DialogDescription>
-                          {dataObject.question}
-                        </DialogDescription>
-                      </DialogHeader>
-
-                      <div className="grid gap-4 py-4">
-                        <div className="flex items-center justify-center w-full">
-                          <Textarea 
-                            value={description}
-                            onChange={(e) => setDescription(e.target.value)}
-                            placeholder="Type your message here." />
-                        </div>
-                      </div>
-
-                      <DialogFooter>
-                        <Button
-                          onClick={() => handleSubmit(dataObject.id)}
-                          type="submit"
-                        >
-                          Save
+                  {/* when formData does not have an existing query */}
+                  {!dataObject.query && (
+                    <Dialog
+                      open={dialogOpenIndex === key}
+                      onOpenChange={(open) =>
+                        setDialogOpenIndex(open ? key : null)
+                      }
+                    >
+                      <DialogTrigger asChild>
+                        <Button className="w-10 h-10 bg-black text-white hover:bg-gray-600">
+                          +
                         </Button>
-                      </DialogFooter>
-                    </DialogContent>
-                  </Dialog>
+                      </DialogTrigger>
+                      <DialogContent>
+                        <DialogHeader>
+                          <DialogTitle>Create Query</DialogTitle>
+                          <DialogDescription>
+                            {dataObject.question}
+                          </DialogDescription>
+                        </DialogHeader>
 
+                        <div className="grid gap-4 py-2">
+                          <div className="flex items-center justify-center w-full">
+                            <Textarea
+                              value={description}
+                              onChange={(e) => setDescription(e.target.value)}
+                              placeholder="Add a new remark..."
+                            />
+                          </div>
+                        </div>
+
+                        <DialogFooter>
+                          <Button
+                            onClick={() => handleSubmit(dataObject.id)}
+                            type="submit"
+                          >
+                            Save
+                          </Button>
+                        </DialogFooter>
+                      </DialogContent>
+                    </Dialog>
+                  )}
                 </div>
               </div>
             </div>
