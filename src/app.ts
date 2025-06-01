a67d14c5c6687cffdc1c2928b0c2087af6b48062
yasaidaisuki <peng.dingkai@gmail.com>
2025-06-01 00:08:03 -0400

    misc

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,18 @@
 import fastify from 'fastify'
 
 import formDataRoutes from './routes/form_data'
+import createQueryRoute from './routes/create_query'
 import errorHandler from './errors'
+import updateQueryRoute from './routes/update_query'
+import deleteQueryRoute from './routes/delete_query'
 
 function build(opts = {}) {
   const app = fastify(opts)
 
   app.register(formDataRoutes, { prefix: '/form-data' })
+  app.register(createQueryRoute, {prefix: '/create-query'})
+  app.register(updateQueryRoute, {prefix: '/update-query'})
+  app.register(deleteQueryRoute, {prefix: '/delete-query'})
 
   app.setErrorHandler(errorHandler)
 
